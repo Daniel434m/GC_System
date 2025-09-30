27faf492d8d43d219b491d13754e5306e3d62fd7
Daniel434m <danielmuteka434@gmail.com>
2025-09-30 21:04:02 +0200

    additional fixes to codespace

--- Index.php
+++ Index.php
@@ -1,4 +1,12 @@
 <?php
-// Load the HTML file without altering it
-readfile("Index.html");
+$file = "index.html";
+
+if (file_exists($file)) {
+    readfile($file);
+} else {
+    http_response_code(404);
+    echo "<h1>404 - File Not Found</h1>";
+    echo "<p>The expected file ($file) is missing.</p>";
+}
 ?>
+
